0e9ff2a38971e3277575d17868027d8c4b2855c9
spencergg <kuidougg@163.com>
2024-03-24 23:42:03 +0800

    Update TestParseApi.php

--- code/TestParseApi.php
+++ code/TestParseApi.php
@@ -1,44 +1,40 @@
 <?php
+    /*
+      请求接口格式：（更详细可参考官网：https://www.resumesdk.com/docs/rs-parser.html#reqType）
+        - uid：必填，用户id；
+        - pwd：必填，用户密码；
+        - file_name: 必填，简历文件名（请确保后缀正确）；
+        - file_cont: 必填，经based64编码的简历文件内容；
+        - need_avatar: 可选，是否需要解析头像，0为不需要，1为需要，默认为0；
+        - 其他可选字段可参考官网：https://www.resumesdk.com/docs/rs-parser.html#reqType
+    */
+    $url ="http://www.resumesdk.com/api/parse";     // 接口地址，也可以用https
 
-$url ="http://www.resumesdk.com/api/parse";
+    $file_name = 'D:/resumeSDK/resume.docx';       // 替换为你的本地文件名
+    $file_cont = base64_encode(file_get_contents($file_name));
+    $data = array(
+        'file_cont' => $file_cont,
+        'file_name'=> $file_name,
+        'need_avatar' => 0
+    );
+    $data_string = json_encode($data);
 
-$fname = 'D:/test_input/resume.docx';		// 替换为你的简历文件名，确保后缀名正确
-$uid = 123;			// 替换为你的用户名(uid)，整数类型
-$pwd = '123abc';		// 替换为你的密码(pwd)，字符串类型
-$fileData = file_get_contents($fname);
+    $headers = array(
+        'Content-Type: application/json',
+        'uid: 123456',      //替换为您的uid
+        'pwd: 123abc'       //替换为您的pwd   
+    );
 
-$base_cont = base64_encode($fileData);
-$data = array(
-    'file_cont' => $base_cont,		// 经base64编码过的文件内容
-    'file_name'=> $fname,					// 文件名
-    'uid' => $uid,					// 用户名
-    'pwd' => $pwd						// 密码
-);
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , $url);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch , CURLOPT_POST, 1);
+    curl_setopt($ch , CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch , CURLOPT_HTTPHEADER, $headers);
+    $output = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-
-$data = json_encode($data, JSON_UNESCAPED_UNICODE);		// 需php5.4及以上才支持JSON_UNESCAPED_UNICODE
-$result = http($url, $data);
-$res_js = json_decode($result, TRUE);
-$name = $res_js['result']['name'];
-var_dump($name);
-//var_dump($res_js);
-//var_dump($result);
-
-exit;
-
-function http($url, $data)
-{
-    $process = curl_init($url);
-	curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	curl_setopt($process, CURLOPT_HEADER, FALSE);
-	curl_setopt($process, CURLOPT_TIMEOUT, 600);
-	curl_setopt($process, CURLOPT_POST, TRUE);
-	curl_setopt($process, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
-	$content = curl_exec($process);
-	curl_close($process);
-
-    return $content;
-}
-
+    echo 'HTTP status code: ' . $httpcode . "\n";
+    print_r($output);
 ?>
